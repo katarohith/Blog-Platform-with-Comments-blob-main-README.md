67ee66f0f0685ab1227d3e4d19ce6b3bcb02fb9d
katarohith <katarohith@karunya.edu.in>
2026-09-08 21:29:04 +0530

    Add JWT authentication middleware

---
+++ authMiddleware.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = function(req,res,next){
+
+    const token = req.header("token");
+
+    if(!token){
+        return res.status(401).json("No Token");
+    }
+
+    try{
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+
+        req.user = verified;
+
+        next();
+    }
+    catch(err){
+        res.status(400).json("Invalid Token");
+    }
+}
